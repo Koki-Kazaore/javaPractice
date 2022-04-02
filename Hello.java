88e822802729d2f12f930aff93cd95118a71cda3
ka300-g <kazaore@icloud.com>
2022-04-03 02:24:17 +0900

    Bランク1問目回答完了しました。

--- Hello.java
+++ Hello.java
@@ -43,7 +43,7 @@ public class Hello {
 
       System.out.println(present);
     }
-    */
+    
 
     // Cランク3問目
     Scanner sc = new Scanner(System.in);
@@ -58,5 +58,53 @@ public class Hello {
 
     int area = D * (D * N - sum);
     System.out.println(area);
+    */
+
+    // Bランク1問目
+    Scanner sc = new Scanner(System.in);
+    
+    /*入力値の代入と配列の作成*/
+    int gondola = sc.nextInt();
+    int group = sc.nextInt();
+
+    int[] gondolaMax = new int[gondola];
+    int[] groupMember = new int[group];
+    int[] gondolaSum = new int[gondola];
+
+    for (int i = 0; i < gondola; i++){
+      gondolaMax[i] = sc.nextInt();
+      gondolaSum[i] = 0;
+    }
+
+    for (int j = 0; j < group; j++){
+      groupMember[j] = sc.nextInt();
+    }
+
+    int gondolaID = 0;
+    // グループの数だけループを回す
+    for (int k = 0; k < group; k++){
+      //System.out.println("(親)ゴンドラID:" + gondolaID);
+      int rider = groupMember[k];
+      // グループのメンバー数がゴンドラのMAX人数を超えた場合は次のゴンドラに移る
+      while (rider > gondolaMax[gondolaID]){
+        gondolaSum[gondolaID] += gondolaMax[gondolaID];
+        rider -= gondolaMax[gondolaID];
+        gondolaID = (gondolaID + 1) % gondola;
+        // System.out.println("(子)ゴンドラID:" + gondolaID);
+      }
+      // グループのメンバーが全員乗り切る処理
+      gondolaSum[gondolaID] += rider;
+      gondolaID = (gondolaID + 1) % gondola;
+    }
+    // 最後に結果を出力
+    for (int l = 0; l < gondola; l++){
+      System.out.println(gondolaSum[l]);
+    }
+
+    /* テスト確認用
+    for (int i = 0; i < gondolaMax.length ; i++) {
+      System.out.print(gondolaMax[i]);
+      }
+    */
   }
 }
